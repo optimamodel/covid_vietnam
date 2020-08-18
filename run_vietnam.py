7aea1877b053665b1920e0d5609a362a5ba3803f
Robyn Stuart <robyna.s@gmail.com>
2020-08-18 12:59:03 +1000

    latest data etc

--- run_vietnam.py
+++ run_vietnam.py
@@ -6,15 +6,18 @@ import numpy as np
 
 def make_sim(beta=None, symp_prob=None, asymp_quar_prob=None):
 
-    end_day = '2020-08-12'
+    end_day = '2020-09-30'
 
-    pars = {'pop_size': 100e3,
-            'pop_infected': 100,
+    pars = {'pop_size': 10e3,
+            'pop_infected': 50,
             'rand_seed': 111,
             'beta': beta,
-            'start_day': '2020-07-20',
+            'start_day': '2020-07-01',
             'end_day': end_day,
             'verbose': .1,
+            'rescale': True,
+            'iso_factor': dict(h=0.3, s=0.01, w=0.01, c=0.1),  # Multiply beta by this factor for people in isolation
+            'quar_factor': dict(h=0.6, s=0.1, w=0.1, c=0.2),  # Multiply beta by this factor for people in quarantine
             'location': 'vietnam',
             'pop_type': 'hybrid'}
 
@@ -38,7 +41,7 @@ T = sc.tic()
 
 # Settings
 doplot = True
-dosave = False
+dosave = True
 
 # Plot settings
 to_plot = sc.objdict({
@@ -46,15 +49,19 @@ to_plot = sc.objdict({
     'Cumulative infections': ['cum_infections'],
     'New infections': ['new_infections'],
     'Daily diagnoses': ['new_diagnoses'],
-#    'Cumulative deaths': ['cum_deaths'],
-#    'Daily deaths': ['new_deaths'],
+    'Cumulative deaths': ['cum_deaths'],
+    'Daily deaths': ['new_deaths'],
     })
 
 # Run and plot
-sim = make_sim(beta=0.025, symp_prob=0.2, asymp_quar_prob=0.1)
+sim = make_sim(beta=0.02, symp_prob=0.12, asymp_quar_prob=0.01)
 sim.run()
-if dosave: sim.save()
+#msim = cv.MultiSim(base_sim=sim)
+#msim.run(n_runs=20, reseed=True, noise=0)
+#msim.reduce()
+
+if dosave: sim.save('vietnam.sim', keep_people=True)
 if doplot: sim.plot(to_plot=to_plot, n_cols=2, do_save=True, do_show=False, fig_path=f'vietnam.png',
-                 legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=5)
+                 legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
 
 sc.toc(T)
