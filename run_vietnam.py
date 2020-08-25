97c178342db9137ffd8038b006d5a7525b7d9b27
Robyn Stuart <robyna.s@gmail.com>
2020-08-25 19:57:03 +1000

    progress to date

--- run_vietnam.py
+++ run_vietnam.py
@@ -3,38 +3,62 @@ import pandas as pd
 import sciris as sc
 import numpy as np
 
-def make_sim(beta=None, symp_prob=None, asymp_quar_prob=None):
+def make_sim():
 
-    end_day = '2020-10-30'
+    start_day = '2020-07-01'
+    end_day = '2020-08-22'
     total_pop = 11.9e6 # Population of central Vietnam
     n_agents = 100e3
     pop_scale = total_pop/n_agents
 
+    # Calibration parameters
+    beta = 0.012
+
     pars = {'pop_size': n_agents,
-            'pop_infected': 5,
+            'pop_infected': 50,
             'pop_scale': pop_scale,
             'rand_seed': 111,
             'beta': beta,
-            'start_day': '2020-07-01',
+            'start_day': start_day,
             'end_day': end_day,
             'verbose': .1,
             'rescale': True,
-            'iso_factor': dict(h=0.3, s=0.01, w=0.01, c=0.1),  # Multiply beta by this factor for people in isolation
-            'quar_factor': dict(h=0.6, s=0.1, w=0.1, c=0.2),  # Multiply beta by this factor for people in quarantine
+            'iso_factor': dict(h=0.5, s=0.01, w=0.01, c=0.1),  # Multiply beta by this factor for people in isolation
+            'quar_factor': dict(h=1.0, s=0.2, w=0.2, c=0.2),   # Multiply beta by this factor for people in quarantine
             'location': 'vietnam',
-            'pop_type': 'hybrid'}
+            'pop_type': 'hybrid',
+            'n_imports': {'dist':'poisson','par1':5.0},
+            'age_imports': [50,80]
+            }
 
-    # Testing and tracing
+    # Make a sim without parameters, just to load in the data to use in the testing intervention and to get the sim days
+    sim = cv.Sim(start_day=start_day, datafile="vietnam_data.csv")
+
+    pars['dur_imports'] = sc.dcp(sim.pars['dur'])
+    pars['dur_imports']['exp2inf']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':0.0}
+    pars['dur_imports']['inf2sym']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':0.0}
+    pars['dur_imports']['sym2sev']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':2.0}
+    pars['dur_imports']['sev2crit'] = {'dist':'lognormal_int', 'par1':1.0, 'par2':3.0}
+    pars['dur_imports']['crit2die'] = {'dist':'lognormal_int', 'par1':3.0, 'par2':3.0}
+
+    # Add testing and tracing interventions
     trace_probs = {'h': 1, 's': 0.95, 'w': 0.8, 'c': 0.3}
     trace_time  = {'h': 0, 's': 2, 'w': 2, 'c': 14}
-    pars['interventions'] = [cv.test_prob(start_day=0, end_day=29, symp_prob=0.15, asymp_quar_prob=asymp_quar_prob, do_plot=False),
-                             cv.test_prob(start_day=30, symp_prob=0.25, asymp_quar_prob=asymp_quar_prob, do_plot=False),
+    pars['interventions'] = [cv.test_num(daily_tests=sim.data['new_tests'], start_day=sim.day('2020-07-01'), symp_test=1.0, do_plot=False),
                              cv.contact_tracing(start_day=0, trace_probs=trace_probs, trace_time=trace_time, do_plot=False),
-                             cv.dynamic_pars({'n_imports': {'days': [15, 20], 'vals': [5, 0]}})
+                             cv.dynamic_pars({'n_imports': {'days': [sim.day('2020-07-15'), sim.day('2020-07-20')], 'vals': [5, 0]}}, do_plot=False)
                              ]
 
-    sim = cv.Sim(pars=pars, datafile="vietnam_data.csv")
 
+
+#    pars['interventions'] = [cv.test_prob(start_day=0, end_day='2020-07-14', symp_prob=0.15, asymp_quar_prob=0.01, do_plot=False),
+#                             cv.test_prob(start_day='2020-07-15', symp_prob=0.25, asymp_quar_prob=0.01, do_plot=False),
+#                             cv.contact_tracing(start_day=0, trace_probs=trace_probs, trace_time=trace_time, do_plot=False),
+                             #cv.dynamic_pars({'n_imports': {'days': [15, 20], 'vals': [5, 0]}})
+#                             ]
+
+
+    sim = cv.Sim(pars=pars, datafile="vietnam_data.csv")
     sim.initialize()
 
     return sim
@@ -45,8 +69,9 @@ def make_sim(beta=None, symp_prob=None, asymp_quar_prob=None):
 T = sc.tic()
 
 # Settings
+domulti = False
 doplot = True
-dosave = True
+dosave = False
 
 # Plot settings
 to_plot = sc.objdict({
@@ -56,18 +81,29 @@ to_plot = sc.objdict({
     'Daily diagnoses': ['new_diagnoses'],
     'Cumulative deaths': ['cum_deaths'],
     'Daily deaths': ['new_deaths'],
+    'Cumulative tests': ['cum_tests'],
+    'Daily tests': ['new_tests'],
     })
 
 # Run and plot
-sim = make_sim(beta=0.016, symp_prob=0.15, asymp_quar_prob=0.01)
+sim = make_sim()
 sim.run()
-#msim = cv.MultiSim(base_sim=sim)
-#msim.run(n_runs=20, reseed=True, noise=0)
-#msim.reduce()
+if domulti:
+    msim = cv.MultiSim(base_sim=sim)
+    msim.run(n_runs=20, reseed=True, noise=0)
+    msim.reduce()
 
-if dosave: sim.save('vietnam.sim', keep_people=True)
-if doplot: sim.plot(to_plot=to_plot, n_cols=2, do_save=True, do_show=False, fig_path=f'vietnam.png',
+if dosave:
+    if domulti: msim.save('vietnam.sim', keep_people=True)
+    else: sim.save('vietnam.sim', keep_people=True)
+if doplot:
+    if domulti:
+        msim.plot(to_plot=to_plot, n_cols=2, do_save=True, do_show=False, fig_path=f'vietnam.png',
                  legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+    else:
+        sim.plot(to_plot=to_plot, n_cols=2, do_save=True, do_show=False, fig_path=f'vietnam.png',
+                 legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+
 
 sc.toc(T)
 
--- vietnam-case-stats.R
+++ vietnam-case-stats.R
@@ -22,11 +22,12 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 ########################################
 # Import and clean data
 ########################################
-rawcases <- read_excel("1_Case and close contact list_Updates_2August.xlsx",  skip = 6)
+rawcases <- read_excel("1_Case and close contact list_Updates_22August_v2.xlsx",  skip = 6)
 rawcases$Region[rawcases$Region=="Highland"] = "Central"
 rawcases$Source[rawcases$Source=="Detected case"] = "Domestic"
 rawcases$Source[rawcases$Source=="Domestic case"] = "Domestic"
 rawcases$Source[rawcases$Source=="Imported case"] = "Imported"
+rawcases$Source = factor(rawcases$Source)
 rawcases$diagdate <- ymd(rawcases$diagdate)
 
 fontfamily="Optima"
@@ -80,12 +81,38 @@ fn <- "cases_by_date_source_region.png"
 ggsave(fn, plot = last_plot(), device="png", dpi=300)
 
 # Make dataframe with data from central region over the past week
-ndf <- data.frame(diagdate = seq(as.Date("2020/7/25"), as.Date("2020/8/2"), "days")) %>%
+diagnoses <- data.frame(diagdate = seq(as.Date("2020/7/1"), as.Date("2020/8/22"), "days")) %>%
   left_join(rawcases %>% filter(Region=="Central", Source=="Domestic") %>% count(diagdate))  %>%
-  rename(date=diagdate,new_diagnoses=n)
+  rename(date=diagdate,new_diagnoses=n) %>% replace(is.na(.), 0)
 
-ndf$new_tests <- c(68,9,0,50,434,351,688,NA,NA) # Manually pulled from testing file -- don't seem right though...?
-write.csv(ndf,"vietnam_data.csv")  
+deaths <- data.frame(Dead = seq(as.Date("2020/7/25"), as.Date("2020/8/22"), "days")) %>%
+  left_join(rawcases %>% filter(Region=="Central", Source=="Domestic") %>% count(Dead))  %>%
+  rename(date=Dead,new_deaths=n) %>% replace(is.na(.), 0)
+
+## Get testing data
+central_codes <- read_excel("Testing_COVID19_HK_22Aug.xlsx", skip = 2) %>%
+  rename(en_name=X__1,Region=X__2) %>%
+  filter(Region=="Central") 
+
+tests <- data.frame(testdate = seq(as.Date("2020/7/1"), as.Date("2020/8/22"), "days"))
+tests$new_tests = NA
+dates <- c("1_7","2_7","3_7","4_7","5_7","6_7","7_7","8_7","9_7","10_7","11_7","12_7","13_7","14_7","15_7","16_7","17_7","18_7","19_7","20_7","21_7","22_7","23_7","24_7","25_7","26_7","27_7","28_7","29_7","30_7","31_7","1_8","2_8","3_8","4_8","5_8","6_8","7_8","8_8","9_8","10_8","11_8","12_8","13_8","14_8","15_8","16_8","17_8","18_8","19_8","20_8","21_8","22_8")
+
+#%>% replace(is.na(.), 0) %>% 
+running_total = c()
+for (d in dates) {
+  if (d %in% c("14_7","20_7","21_7")) {todays_total <- 0}
+  else {
+    todays_tests <- read_excel("Testing_COVID19_HK_22Aug.xlsx",  sheet = d, skip = 2) %>% 
+    filter(`Đơn vị thực hiện` %in% central_codes$`Names of health facilities`) 
+  todays_total <- sum(todays_tests$`Kết quả trong ngày`)}
+  running_total = c(running_total, todays_total)
+}
+
+vietnam_data <- left_join(diagnoses, deaths)
+vietnam_data$new_tests <- running_total
+write.csv(vietnam_data,"vietnam_data.csv")  
+
 
 ########################################
 # Investigating clusters... 
