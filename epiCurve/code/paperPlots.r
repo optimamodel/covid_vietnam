d6edbb2e0a02761e68ef630504619b34d140ebd9
Assaf Oron <assaf.oron@gmail.com>
2020-09-23 10:48:12 -0700

    First run-through after move.... don't look yet!

--- epiCurve/code/paperPlots.r
+++ epiCurve/code/paperPlots.r
@@ -1,8 +1,8 @@
+library(ggplot2)
 library(plyr)
 library(tools)
 library(data.table)
 #library(EpiEstim)
-library(ggplot2)
 library(scales)
 
 cat(date(),'\n')
@@ -28,75 +28,74 @@ blank=guides(fill=guide_legend(title=NULL),color=FALSE)
 milearrow<-function(dat,grace=-0.,col1='grey60') 
 {
 require(ggplot2)
-geom_curve(data=dat,aes( x = x1, y=y1, xend = miledate, yend = y2), curvature=grace, color = col1, arrow = arrow(length = unit(2, "mm"))) 
+geom_curve(data=dat,aes( x = x1, y=y1, xend = date, yend = y2), curvature=grace, color = col1, arrow = arrow(length = unit(2, "mm"))) 
 }
-miletext<-function(dat,psize=4) 
+miletext<-function(dat,psize=4,colr='black') 
 {
 require(ggplot2)
-geom_text(data=dat,aes( x = x0, y = y0, label = milestone,color=colcode), hjust = "left",lineheight = 0.9, size=psize) 
+geom_text(data=dat,aes( x = x0, y = y0, label = milestone), col=colr,hjust = 0.5, size=psize) 
 }
   
 
 ###################################################### Read
 
 quang=fread('../data/Case and close contact list_Updates_22Sep2020.csv',skip=6)
+provinces=fread('../data/vietProvinces.csv')
+miles=fread('../data/Milestones_updates_Revised_3.csv')
+
 #quang[,CaseID:=as.integer(gsub('NB','',CaseID))]
-# Imported vs. Domestic, cognizant of typos ;)
+
+## Imported vs. Domestic, cognizant of typos ;)
 quang[,domestic:=grepl('D',Case_cat)]
 
 #quang[,Gender:=tolower(Gender)]
 #setkey(quang,CaseID)
 
-### Regions!
-provinces=fread('../data/vietProvinces.csv')
+#### Regions!
 provinces[,Province:=toTitleCase(Province)]
 quang[,Province:=toTitleCase(Province)]
 quang[,Region:=provinces$Region[match(Province,provinces$Province)]]
+
 # Errata etc.
 quang[Province %in% c('Hanoi','Bac Can'),Region:='North']
 quang[Province %in% c('TPHCM','BR-VT'),Region:='South']
 quang[Province %in% c('Da Nang','Quang Tri'),Region:='Central']
 quang[Province %in% c('Daklak'),Region:='Highland']
-### filling in blank entries
-quang[Province=='' & Dis=='Nghia Tan',Region:='North']
-quang[Province=='' & detected_place %in% c('Phu Tho','Khu cach ly tap trung HN'),Region:='North']
-# These are mostly the hospital cluster:
-quang[Province=='' & Country=='Ha Noi',Region:='North']
-# Last 2 lines are completely ad-hoc
-quang[is.na(Region) & grep(203,ID),Region:='South']
-quang[is.na(Region),Region:='North']
 
+# Current region name conventions:
+quang[,Region:=gsub('North','Northern',Region)]
+quang[,Region:=gsub('South','Southern',Region)]
 
 ### other cleaning
 # date mess (some American, some old-world)
 quang[,dxdate:=as.Date(diagdate,'%m/%d/%Y')]
 quang[is.na(dxdate),dxdate:=as.Date(diagdate,'%d/%m/%Y')]
 
-fwrite(quang,file=paste('../output/VietnamLine',dim(quang)[1],'.csv',sep=''))
+#fwrite(quang,file=paste('../output/VietnamLine',dim(quang)[1],'.csv',sep=''))
+
+##### Milestone data prep
+
+# Current region name conventions:
+setnames(miles,'Level','Region')
+miles[,Region:=gsub('North','Northern',Region)]
+miles[,Region:=gsub('South','Southern',Region)]
+
+miles[,date:=as.Date(date,'%m/%d/%Y')]
+miles[,x0:=date]
+miles[,x1:=date]
+
+miles[,y0:=40]
+miles[,y1:=35]
+miles[,y2:=10]
+
 
 ################################################# Plotting
+# Epi curve dataset format
 vietnamEpi=quang[Region!='Highland',list(newcases=.N),keyby=.(dxdate,Region,domestic)]
 
-### Milestone data
-miles=fread('../data/vietMilestonesUpdate_202009.csv')
-miles[,miledate:=as.Date(miledate,'%m/%d/%Y')]
-miles[,x0:=as.Date(x0,'%m/%d/%Y')]
-miles[,x1:=as.Date(x1,'%m/%d/%Y')]
-miles[,colcode:=(reg=='All')]
 
-milesref=expand.grid(milestone=miles$milestone[miles$reg=='All'],Region=c('North','Central','South'))
 
-milesall=merge(miles,milesref,by='milestone',all=TRUE)
-setDT(milesall)
-
-milesall[is.na(Region),Region:=reg]
-milesall[is.na(x0),x0:=miledate-7]
-milesall[is.na(x1),x1:=miledate]
-milesall[is.na(y0),y0:=40]
-milesall[is.na(y1),y1:=35]
-milesall[is.na(y2),y2:=10]
-
-p1<-ggplot(vietnamEpi,aes(x=dxdate,y=newcases))+ geom_col(aes(fill=factor(domestic,labels=c('Imported','Domestic')))) +mycols+yname+dates+dates2+xlab('')+overall+blank+facet_grid(relevel(factor(Region),'North')~.) +milearrow(milesall)+miletext(milesall)+scale_color_manual(values=c('red','black'))
+p1<-ggplot(vietnamEpi,aes(x=dxdate,y=newcases))+ geom_col(aes(fill=factor(domestic,labels=c('Imported','Domestic')))) +mycols+yname+dates+dates2+xlab('')+overall+blank+facet_grid(relevel(factor(Region),'Northern')~.) +milearrow(miles[Region!='National',])+miletext(miles[Region!='National',])+scale_color_manual(values=c('red','black'))
 
 ggsave(p1,file='../output/paperPlot1.png',height=fhigh,width=fwide)
 
