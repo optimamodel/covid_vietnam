30db5d4623e2dfbfbe24e0477af6f00362250132
Assaf Oron <assaf.oron@gmail.com>
2020-09-23 19:34:02 -0700

    dun for today

--- epiCurve/code/paperPlots.r
+++ epiCurve/code/paperPlots.r
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(plyr)
 library(tools)
-library(gridExtra)
+library(cowplot)
 library(data.table)
 #library(EpiEstim)
 library(scales)
@@ -16,12 +16,12 @@ fhigh=10
 f2wide=12	
 f2high=12
 psize=5
-linelength=35
+linelength=32
 lheight=0.7
 tcol='black'
 mycols=scale_fill_manual(values=c("grey30","darkgoldenrod2","darkcyan"))
 yname=ylab("New Confirmed Cases")
-dates=function(dat) scale_x_date(date_breaks ="week",limits=range(dat$date),labels = date_format("%d-%b"),expand=c(0.1,0.1))
+dates=function(dat) scale_x_date(date_breaks ="week",limits=range(dat$date),breaks=seq(as.Date('2020-1-6'),as.Date('2020-9-21'),7),labels = date_format("%d-%b"),expand=c(0.1,0.1))
 dates2=theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 overall=theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),legend.position = c(0.93,0.94))
 blank=guides(fill=guide_legend(title=NULL),color=FALSE)
@@ -29,19 +29,19 @@ textcol=scale_color_manual(values=c('black','red','blue'))
 
 ## Plotting utilities
 
-milearrow<-function(dat,grace=-0.,col1='grey60') 
+milearrow<-function(dat,grace=-0.,col1='grey60',...) 
 {
 require(ggplot2)
-geom_curve(data=dat,aes( x = x1, y=y1, xend = date, yend = y2,color=factor(colcode)), curvature=grace, arrow = arrow(length = unit(2, "mm"))) 
+geom_curve(data=dat,aes( x = x1, y=y1, xend = date, yend = y2,color=factor(colcode)), curvature=grace, arrow = arrow(length = unit(2, "mm")),alpha=0.8,...) 
 }
-miletext<-function(dat,psize=3.5,colr='black') 
+miletext<-function(dat,psize=4,colr='black',...) 
 {
 require(ggplot2)
-geom_text(data=dat,aes( x = x0, y = y0, label = milestone,color=factor(colcode)), lineheight = lheight, size=psize,hjust = 0.5,vjust=0.5) 
+geom_label(data=dat,aes( x = x0, y = y0, label = milestone,color=factor(colcode)), lineheight = lheight, size=psize,hjust = 0.5,vjust=0.5,...) 
 }
   
 
-###################################################### Read
+################################################################# Read
 
 quang=fread('../data/Case and close contact list_Updates_22Sep2020.csv',skip=6)
 provinces=fread('../data/vietProvinces.csv')
@@ -80,30 +80,51 @@ quang[is.na(dxdate),dxdate:=as.Date(diagdate,'%d/%m/%Y')]
 # Epi curve dataset format
 vietnamEpi=quang[Region!='Highland',list(newcases=.N),keyby=.(dxdate,Region,domestic)]
 
-##### Milestone data prep
+########################################## Milestone label prep
+miles[,date:=as.Date(date,'%m/%d/%Y')]
+setkey(miles,date)
 
 # Current region name conventions:
 setnames(miles,'Level','Region')
 miles[,Region:=gsub('North','Northern',Region)]
 miles[,Region:=gsub('South','Southern',Region)]
 
-miles[,date:=as.Date(date,'%m/%d/%Y')]
-setkey(miles,date)
-miles[,x0:=date]
-# Danang
-miles[grep('99',milestone),x0:=x0-50]
-# Edge effects?
-miles[date<as.Date('2020-2-1'),x0:=x0+7]
-#miles[date>as.Date('2020-9-10'),x0:=x0-7]
-miles[grep('Targeted lockdown',milestone),x0:=x0+14]
-miles[grep('Lockdown',milestone),x0:=x0+14]
-miles[,x1:=x0]
+# errata
+miles[,milestone:=gsub('dearpart','depart',milestone)]
+miles[,milestone:=gsub('Contract','Contact',milestone)]
+miles[,milestone:=gsub('Commuity','Community',milestone)]
 
-miles[,y0:=rep(seq(50,20,-10),1+.N/4)[1:.N],by='Region']
+############## X coordinate
+miles[,x0:=date]
+### Danang, shifting x coords around wave
+miles[grep('99',milestone),x0:=x0-40]
+miles[milestone=='Ban on public gatherings in Danang',x0:=x0-25]
+miles[grep('Community search',milestone),x0:=x0+30]
+miles[grep('Danang citywide',milestone),x0:=x0-20]
+
+# Edge effects?
+#miles[date<as.Date('2020-2-1'),x0:=x0+7]
+#miles[date>as.Date('2020-9-10'),x0:=x0-7]
+miles[grep('north of Hanoi',milestone),x0:=x0-10]
+
+miles[grep('Targeted lockdown',milestone),x0:=x0+7]
+miles[grep('Lockdown',milestone),x0:=x0+14]
+
+### Arrow origin's x coordinate
+miles[,x1:=x0]
+# And... Danang, where else
+miles[grep('Community search',milestone),x1:=x1-28]
+miles[grep('Danang citywide',milestone),x1:=x1+15]
+
+############# Y coordinate, generating "ladder"
+
+miles[,y0:=rep(seq(52,22,-10),1+.N/4)[1:.N],by='Region']
 miles[Region=='National',y0:=rep(seq(40,16,-4),1+.N/7)[1:.N]]
-# Danang outbreak
-miles[Region=='Central' & date>as.Date('2020-7-1'),y0:=y0+30]
-miles[grep('99',milestone),y0:=y0-20]
+### Danang, shifting y coords around wave
+miles[Region=='Central' & date>as.Date('2020-7-1'),y0:=y0+10]
+miles[grep('99',milestone),y0:=y0-30]
+miles[grep('Danang citywide',milestone),y0:=y0-10]
+
 
 miles[,y1:=y0-5]
 miles[y1<5,y1:=5]
@@ -112,7 +133,7 @@ miles[y1<5,y1:=5]
 episums=vietnamEpi[,list(allcases=sum(newcases)),keyby=.(dxdate,Region)]
 miles[,cases:=episums$allcases[match(paste(date,Region),paste(episums$dxdate,episums$Region))]]
 miles[is.na(cases),cases:=0]
-miles[,y2:=cases+1]
+miles[,y2:=cases+2]
 
 # line breaks?
 miles[nchar(milestone)>linelength,milestone:=gsub(paste('(.{',linelength,'})(\\s)',sep=''), '\\1\n',milestone)]
@@ -123,31 +144,27 @@ miles[grep('[Ll]ockdown',milestone),colcode:=2]
 miles[grep('[Rr]elax',milestone),colcode:=3]
 miles[grep('reopen',milestone),colcode:=3]
 
-miles[Region=='National' & colcode==2,y0:=50]
-miles[Region=='National' & colcode==3,y0:=45]
-miles[Region=='National' & colcode>1,y1:=y0+5]
+miles[Region=='National' & colcode>1,y0:=50]
+miles[Region=='National' & grepl('Rural',milestone),y0:=45]
+#miles[Region=='National' & colcode==3,y0:=45]
+miles[Region=='National',y1:=y0+2]
 miles[Region=='National',y2:=60]
 ################################################# Plotting
 
 
 
-p1<-ggplot(vietnamEpi,aes(x=dxdate,y=newcases))+ geom_col(width=1,aes(fill=factor(domestic,labels=c('Imported','Domestic')))) +mycols+yname+dates(miles)+dates2+xlab('')+overall+blank+facet_grid(relevel(factor(Region),'Northern')~.) +milearrow(miles[Region!='National',])+miletext(miles[Region!='National',])+textcol+theme(plot.margin=unit(c(.2,.2,0,.2),"cm"))
+p1<-ggplot(vietnamEpi,aes(x=dxdate,y=newcases))+ geom_col(width=1,aes(fill=factor(domestic,labels=c('Imported','Domestic')))) +mycols+yname+dates(miles)+dates2+xlab('')+overall+blank+facet_grid(relevel(factor(Region),'Northern')~.) +milearrow(miles[Region!='National',])+miletext(miles[Region!='National',])+textcol+theme(plot.margin=unit(c(.002,.002,-0.03,.002),"npc"))
 
 
 
-p2<-ggplot(miles[Region=='National',],aes(label=milestone,x=date,y=y0,color=factor(colcode)))+geom_label(hjust=0.5,vjust=0.5,size=3.,lineheight=lheight)+dates(miles)+dates2+textcol+blank+scale_y_continuous(limits=c(12,60),expand=c(0,0))+milearrow(miles[Region=='National' & colcode>1,])+theme_void()+theme(plot.margin=unit(c(0,.2,.2,.2),"cm"))
+p2<-ggplot(miles[Region=='National',],aes(label=milestone,x=date,y=y0,color=factor(colcode)))+ milearrow(miles[Region=='National',])+geom_label(hjust=0.5,vjust=0.5,size=3.,lineheight=lheight)  + dates(miles)+dates2+textcol+blank+scale_y_continuous(limits=c(12,60),expand=c(0,0))+theme_void()+theme(plot.margin=unit(c(0,.2,.2,0.2),"cm"))
 #plot.background = element_rect(fill = "grey90"))
 
-ggsave(grid.arrange(p1,p2,heights=c(3.5,1),padding=0),file='../output/paperPlot1.png',height=13,width=15)
+ggsave(plot_grid(p1,p2,rel_heights=c(3.5,1),align='v',axis='rl',ncol=1),file='../output/paperPlot1.png',height=13,width=15)
+#ggsave(ggarrange(p1,p2,heights=c(3.5,1),padding=0,align='v'),file='../output/paperPlot1.png',height=13,width=15)
 
 
-
-
-
-
-
-
-	
+cat(date(),'\n')
 
 
 
