03f3ed08f6cd6cbf36cb57d2233e4f4530b9c17a
Assaf Oron <assaf.oron@gmail.com>
2020-09-25 11:03:13 -0700

    milestones update 4

--- epiCurve/code/paperPlots.r
+++ epiCurve/code/paperPlots.r
@@ -23,7 +23,7 @@ mycols=scale_fill_manual(values=c("grey30","darkgoldenrod2","darkcyan"))
 yname=ylab("New Confirmed Cases")
 dates=function(dat) scale_x_date(date_breaks ="week",limits=range(dat$date),breaks=seq(as.Date('2020-1-6'),as.Date('2020-9-21'),7),labels = date_format("%d-%b"),expand=c(0.1,0.1))
 dates2=theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-overall=theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),legend.position = c(0.93,0.94))
+overall=theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),legend.position = c(0.93,0.25),legend.background = element_rect(linetype=1))
 blank=guides(fill=guide_legend(title=NULL),color=FALSE)
 textcol=scale_color_manual(values=c('black','red','blue'))
 
@@ -45,7 +45,7 @@ geom_label(data=dat,aes( x = x0, y = y0, label = milestone,color=factor(colcode)
 
 quang=fread('../data/Case and close contact list_Updates_22Sep2020.csv',skip=6)
 provinces=fread('../data/vietProvinces.csv')
-miles=fread('../data/Milestones_updates_Revised_3.csv')
+miles=fread('../data/Milestones_updates_Revised_4.csv')
 
 #quang[,CaseID:=as.integer(gsub('NB','',CaseID))]
 
@@ -81,6 +81,7 @@ quang[is.na(dxdate),dxdate:=as.Date(diagdate,'%d/%m/%Y')]
 vietnamEpi=quang[Region!='Highland',list(newcases=.N),keyby=.(dxdate,Region,domestic)]
 
 ########################################## Milestone label prep
+
 miles[,date:=as.Date(date,'%m/%d/%Y')]
 setkey(miles,date)
 
@@ -95,14 +96,26 @@ miles[,milestone:=gsub('Contract','Contact',milestone)]
 miles[,milestone:=gsub('Commuity','Community',milestone)]
 miles[,milestone:=gsub('detected Hai','detected in Hai',milestone)]
 miles[,milestone:=gsub('Flights ban','Flight ban',milestone)]
-
-############## X coordinate
+miles[,milestone:=gsub('A city','Danang city',milestone)]
+miles[milestone=='Ban on public gatherings',milestone:=paste(milestone,'in Danang')]
+miles[milestone=='Lockdown relaxed',milestone:=paste('Danang',tolower(milestone))]
+miles[milestone=='Lockdown 3 hospitals with case detection and surrounding house blocks',
+	milestone:='Lockdown 3 hospitals and surrounding house blocks, with case detection']
+##################### X coordinate
 miles[,x0:=date]
+
+
 ### Danang, shifting x coords around wave
-miles[grep('99',milestone),x0:=x0-40]
-miles[milestone=='Ban on public gatherings in Danang',x0:=x0-25]
-miles[grep('Community search',milestone),x0:=x0+30]
-miles[grep('Danang citywide',milestone),x0:=x0-20]
+miles[grep('99',milestone),x0:=x0-50]
+miles[milestone=='Ban on public gatherings in Danang',x0:=x0-30]
+miles[grep('Community active',milestone),x0:=x0+30]
+miles[grep('Lockdown 3',milestone),x0:=x0-50]
+miles[grep('city lockdown',milestone),x0:=x0-14]
+#miles[grep('99',milestone),x0:=x0-50]
+
+# One in Northern
+miles[grep('Vinh Phuc',milestone),x0:=x0-15]
+
 
 # Edge effects?
 #miles[date<as.Date('2020-2-1'),x0:=x0+7]
@@ -115,24 +128,29 @@ miles[grep('Lockdown',milestone),x0:=x0+14]
 ### Arrow origin's x coordinate
 miles[,x1:=x0]
 # And... Danang, where else
-miles[grep('Community search',milestone),x1:=x1-28]
-miles[grep('Danang citywide',milestone),x1:=x1+15]
+miles[grep('Community active',milestone),x1:=x1-28]
+miles[grep('city lockdown',milestone),x1:=x1+12]
 
 ############# Y coordinate, generating "ladder"
 
-miles[,y0:=rep(seq(52,22,-10),1+.N/4)[1:.N],by='Region']
+miles[,y0:=rep(seq(55,25,-10),1+.N/4)[1:.N],by='Region']
 miles[Region=='National',y0:=rep(seq(40,16,-4),1+.N/7)[1:.N]]
+
 ### Danang, shifting y coords around wave
 miles[Region=='Central' & date>as.Date('2020-7-1'),y0:=y0+10]
-miles[grep('99',milestone),y0:=y0-30]
+miles[grep('99',milestone),y0:=y0-20]
+miles[milestone=='Ban on public gatherings in Danang',y0:=y0+10]
 miles[grep('Danang citywide',milestone),y0:=y0-10]
+miles[grep('Community active',milestone),y0:=y0+10]
+miles[grep('One person',milestone),y0:=y0+5]
+miles[milestone=='Danang lockdown relaxed',y0:=20]
 
-### Adjusting y for isolated milestones
+### Adjusting y for isolated milestones (so they're not "hanging high")
 
 miles[,dist1:=c(linelength,diff(x0)),by='Region']
 miles[,dist2:=c(diff(x0),linelength),by='Region']
 miles[Region=='National' & pmin(dist1,dist2)>=linelength,y0:=40]
-miles[Region!='National' & pmin(dist1,dist2)>=linelength,y0:=30]
+miles[Region!='National' & pmin(dist1,dist2)>=linelength,y0:=35]
 
 
 miles[,y1:=y0-5]
@@ -151,7 +169,7 @@ miles[nchar(milestone)>linelength,milestone:=gsub(paste('(.{',linelength,'})(\\s
 miles[,colcode:=1]
 miles[grep('[Ll]ockdown',milestone),colcode:=2]
 miles[grep('[Rr]elax',milestone),colcode:=3]
-miles[grep('reopen',milestone),colcode:=3]
+miles[grep('[Rr]eopen',milestone),colcode:=3]
 
 
 miles[Region=='National' & colcode>1,y0:=50]
