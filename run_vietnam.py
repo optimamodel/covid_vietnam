db49a90158f09998fdf45e85ecb07539de292f27
Robyn Stuart <robyna.s@gmail.com>
2020-11-09 11:18:00 +0100

    major updates

--- run_vietnam.py
+++ run_vietnam.py
@@ -1,5 +1,5 @@
 '''
-Run Vietnam
+Run Vietnam scenarios for the whole country
 '''
 
 import covasim as cv
@@ -10,17 +10,17 @@ today = '2020-10-12'
 
 def make_sim():
 
-    start_day = '2020-06-15'
-    end_day = '2020-10-12'
-    total_pop = 11.9e6 # Population of central Vietnam
+    start_day = '2020-10-15'
+    end_day = '2021-06-30'
+    total_pop = 95.5e6 # Population of central Vietnam
     n_agents = 100e3
     pop_scale = total_pop/n_agents
 
     # Calibration parameters
     pars = {'pop_size': n_agents,
-            'pop_infected': 10,
+            'pop_infected': 1,
             'pop_scale': pop_scale,
-            'rand_seed': 1, #15 #1 #53 #64 #80 #81 #82 #95 #96
+            'rand_seed': 1,
             'beta': 0.0145,
             'start_day': start_day,
             'end_day': end_day,
@@ -30,38 +30,18 @@ def make_sim():
             'quar_factor': dict(h=1.0, s=0.2, w=0.2, c=0.2),   # Multiply beta by this factor for people in quarantine
             'location': 'vietnam',
             'pop_type': 'hybrid',
-#            'n_imports': {'dist': 'poisson', 'par1': 5.0},
-            'age_imports': [50,80],
-            'rel_crit_prob': 1.25, # Calibration parameter due to hospital outbreak
-            'rel_death_prob': 1.25, # Calibration parameter due to hospital outbreak
             }
-
-    # Make a sim without parameters, just to load in the data to use in the testing intervention and to get the sim days
-    sim = cv.Sim(start_day=start_day, datafile="vietnam_data.csv")
-
-    pars['dur_imports'] = sc.dcp(sim.pars['dur'])
-    pars['dur_imports']['exp2inf']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':0.0}
-    pars['dur_imports']['inf2sym']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':0.0}
-    pars['dur_imports']['sym2sev']  = {'dist':'lognormal_int', 'par1':0.0, 'par2':2.0}
-    pars['dur_imports']['sev2crit'] = {'dist':'lognormal_int', 'par1':1.0, 'par2':3.0}
-    pars['dur_imports']['crit2die'] = {'dist':'lognormal_int', 'par1':3.0, 'par2':3.0}
 
     # Add testing and tracing interventions
     trace_probs = {'h': 1, 's': 0.95, 'w': 0.8, 'c': 0.05}
     trace_time  = {'h': 0, 's': 2, 'w': 2, 'c': 5}
     pars['interventions'] = [
-        # Testing and tracing
-        cv.test_num(daily_tests=sim.data['new_tests'], start_day=sim.day('2020-07-01'), end_day=sim.day('2020-08-22'), symp_test=60.0, quar_test=50.,do_plot=False),
-        cv.test_num(daily_tests=7000, start_day=sim.day('2020-08-23'), symp_test=60.0, quar_test=50.,do_plot=False),
+        cv.test_prob(start_day=0, symp_prob=0.2, asymp_quar_prob=0.2, do_plot=False),
         cv.contact_tracing(start_day=0, trace_probs=trace_probs, trace_time=trace_time, do_plot=False),
-
-        # Introduce imported cases in mid-July and then again in September (representing reopening borders)
-        cv.dynamic_pars({'n_imports': {'days': [sim.day('2020-07-20'), sim.day('2020-07-25')], 'vals': [20, 0]}}, do_plot=False),
-        #cv.dynamic_pars({'n_imports': {'days': [sim.day('2020-10-01'), sim.day('2020-10-02')], 'vals': [10, 0]}}, do_plot=False),
 
         # Increase precautions (especially mask usage) following the outbreak, which are then abandoned after 40 weeks of low case counts
 #        cv.change_beta(['2020-07-30'], [0.25]),
-        cv.change_beta(days=0, changes=0.25, trigger=cv.trigger('date_diagnosed',5)),
+#        cv.change_beta(days=0, changes=0.25, trigger=cv.trigger('date_diagnosed',5)),
         #cv.change_beta(days=80, changes=1.0, trigger=cv.trigger('date_diagnosed', 2, direction='below', smoothing=28)),
 #        cv.change_beta(days=108, changes=0.4, trigger=cv.trigger('date_diagnosed', 5)),
 
@@ -69,14 +49,12 @@ def make_sim():
 #        cv.dynamic_pars({'rel_death_prob':{'days':sim.day('2020-08-31'), 'vals':1.0},'rel_crit_prob':{'days':sim.day('2020-08-31'), 'vals':1.0}}) # Assume these were elevated due to the hospital outbreak but then would return to normal
         ]
 
-    sim = cv.Sim(pars=pars, datafile="vietnam_data.csv")
+    sim = cv.Sim(pars=pars)
     sim.initialize()
 
     return sim
 
-
 # Run
-
 T = sc.tic()
 cv.check_save_version()
 
@@ -84,8 +62,8 @@ cv.check_save_version()
 do_multi = True
 do_plot = True
 do_save = True
-save_sim = True
-n_runs = 6
+save_sim = False
+n_runs = 15
 
 # Plot settings
 to_plot = sc.objdict({
@@ -97,8 +75,6 @@ to_plot = sc.objdict({
     'Daily deaths': ['new_deaths'],
     'Cumulative tests': ['cum_tests'],
     'Daily tests': ['new_tests'],
-#    'Test yield': ['test_yield'],
-#    'Number quarantined': ['n_quarantined'],
     })
 
 # Run and plot
@@ -109,10 +85,10 @@ if do_multi:
     msim.run(n_runs=n_runs)
     msim.reduce()
     if do_plot:
-        msim.plot(to_plot=to_plot, do_save=do_save, do_show=False, fig_path=f'vietnam.png',
+        msim.plot(to_plot=to_plot, do_save=do_save, do_show=False, fig_path=f'vietnam_scens.png',
                   legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
     if save_sim:
-        msim.save('vietnam_sim.obj')
+        msim.save('vietnam_scens.obj')
 
 #if domulti:
 #    s0.run(until=today)
@@ -133,6 +109,9 @@ else:
     s0.run()
     if save_sim:
         s0.save('vietnam_sim.obj')
+    if do_plot:
+        s0.plot(to_plot=to_plot, do_save=do_save, do_show=False, fig_path=f'vietnam.png',
+                  legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
 
 
 sc.toc(T)
